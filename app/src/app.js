8dd778e95966aa68004ded622583ec4f8b772aea
alexpud <alexandre.maciele@gmail.com>
2017-03-30 16:30:28 -0300

    Final commit

--- app/src/app.js
+++ app/src/app.js
@@ -1,48 +1,36 @@
- var i=7;
+
 function addEventListener(){
-  console.log("here");
   var div=document.getElementById('card_area1');
+  div.addEventListener("click",
+    function(rawElement) {
 
-  div.addEventListener("click", function(rawElement){
-    console.log(rawElement);
-    var element = rawElement.srcElement,
-      currentElement = {};
+      var element = rawElement.srcElement,
+        currentElement = {};
 
-    if( element.className === "fa fa-times"){
-      parentElement = rawElement.srcElement.parentElement;
+      if( element.className === "fa fa-times"){
+        parentElement = rawElement.srcElement.parentElement;
 
-      while(parentElement.className != "card_area"){
-        if(parentElement.className == "card")
-          currentCard = parentElement;
-        parentElement = parentElement.parentElement;
+        while(parentElement.className != "card_area"){
+          if(parentElement.className == "card")
+            currentCard = parentElement;
+
+          parentElement = parentElement.parentElement;
+        }
+        parentElement.removeChild(currentCard);
       }
 
-      parentElement.removeChild(currentCard);
-    }
+      else if ( element.className === "fa fa-plus-circle"){
+        parentElement = rawElement.srcElement.parentElement;
 
-    else if ( element.className === "fa fa-plus-circle"){
-      parentElement = rawElement.srcElement.parentElement;
+        while(parentElement.className != "card_area"){
+          if(parentElement.className == "card")
+            currentCard = parentElement;
 
-      while(parentElement.className != "card_area"){
-        if(parentElement.className == "card")
-          currentCard = parentElement;
-        parentElement = parentElement.parentElement;
+          parentElement = parentElement.parentElement;
+        }
+        addCard(parentElement);
       }
-      console.log(parentElement);
-      addCard(parentElement);
-    }
-  });
-}
-
-function createItem(){
-    var div=document.getElementsByClassName('card_area');
-    console.log(div.innerHTML);
-}
-
-function removeCard(id){
-    var div=document.getElementById('card_area1');
-    var element=document.getElementById(id);
-    div.removeChild(element);
+    });
 }
 
 function addCard(cardAreaDiv){
@@ -50,12 +38,11 @@ function addCard(cardAreaDiv){
       node=document.createElement("DIV");
 
     node.className="card";
-    node.id="card"+i;
     node.innerHTML += ' <div class="card_info"> \
-                        <img \
-                            src="static/helsinki-card (1).png" \
-                            alt="Smiley face" height="230" width="280" \
-                        > \
+                          <img class = "card_image" class = "card_image" \
+                              src="static/helsinki-card (1).png" \
+                              alt="Smiley face" height="230" width="280" \
+                          > \
                     <div class="status"> \
                         <div class="back_btn"> \
                         <i class="fa fa-undo" aria-hidden="true"></i> \
@@ -71,7 +58,7 @@ function addCard(cardAreaDiv){
                         </div> \
                     </div> \
                 </div> \
-                <div class="card_name_area" id="card"> \
+                <div class="card_name_area"> \
                     <div class="card_name"> \
                         <i class="fa fa-user-circle" aria-hidden="true"> \
                             Jhon \
